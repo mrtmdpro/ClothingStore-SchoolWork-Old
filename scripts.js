dd7792f20bbaa31f76ddf02be92b989975a3d2b5
= <minhducta4@gmail.com>
2021-11-04 19:39:15 +0300

    Fixed spredsheet

--- scripts.js
+++ scripts.js
@@ -3,18 +3,16 @@
 const e = Spreadsheet('#billingsheet');
 e.createSpreadsheet(
     {
-        bank: 'text',
-        'cash ($)': 'number',
-        'credit debt ($)': 'number',
-        'investment portfolio ($)': 'number',
-        city: 'text',
-        'dated opened': 'date',
+        'Товар': 'text',
+        'Стоимость ($)': 'number',
+        'Количество': 'number',
+        'Сумма ($)': 'number',
     },
     {
         data: [
-            ['TD', '10000', '1000000', '34535', 'Toronto', '2021-04-06'],
-            ['RBC', '50000', '325345', '76868', 'Markham', '2020-06-22'],
-            ['AmEx', '10000000', '5644', '76054', 'New York City', '2020-07-06'],
+            ['TD', '10000', '1000000', '34535'],
+            ['RBC', '50000', '325345', '76868'],
+            ['AmEx', '10000000', '5644', '76054'],
         ],
     }
 );
