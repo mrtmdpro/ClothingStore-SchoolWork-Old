b109c642662d3c6ffb8f546873172e827178d15c
= <minhducta4@gmail.com>
2021-12-18 02:49:24 +0300

    added insert into table

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('../db/orders.db');
+let db = new sqlite3.Database('./db/orders.db');
 
 db.run('CREATE TABLE orders (id INTEGER PRIMARY KEY AUTOINCREMENT, fname VARCHAR(20), lname VARCHAR(20), pname VARCHAR(20), tel VARCHAR(12), email VARCHAR(50), country VARCHAR(20), typeofshipping INTEGER, dateofreceive DATE, comment TEXT )');
 
